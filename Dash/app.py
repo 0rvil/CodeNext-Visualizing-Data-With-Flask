9cd67e2252ab9323cfc42abd1524a0010f3e2a78
0rvil <orvile@csedge.org>
2023-05-30 19:48:57 -0700

    Updated Dash import statements to match latest version; Added CSS; and added a Nav Bar

--- Dash/app.py
+++ Dash/app.py
@@ -1,6 +1,6 @@
 import dash
-import dash_html_components as html
-import dash_core_components as dcc
+from dash import html
+from dash import dcc
 import plotly.express as px
 import pandas as pd
 
@@ -8,36 +8,16 @@ app = dash.Dash(__name__)
 
 df = pd.DataFrame({
     "Fruit": ["Apples", "Oranges", "Bananas", "Mangos", "Watermelons", "Grapes"],
-    "Cost": [4, 1, 2, 2, 4, 50],
+    "Cost": [4, 1, 25, 2, 4, 50],
 })
 #Gamestop dataframe
 gme_df = pd.read_csv('../Data/gme_data.csv')
 
-#TMDB 5000 movie dataset
-m_df = pd.read_csv('../Data/tmdb_5000_movies.csv')
-
 def get_value(row):
     return row[1]
-    
-# Date 01/01/2000
-m_df['Date_dt'] = pd.to_datetime(m_df['release_date'], format='%Y/%m/%d')
-# Movies by month
-m_df['Month'] = m_df['Date_dt'].dt.month
-m_df['Year'] = m_df['Date_dt'].dt.year
-
-
 
 gme_df['Date_dt'] = pd.to_datetime(gme_df['Date'], format='%m/%d/%Y')
 gme_df['Close_num'] = pd.to_numeric(gme_df['Close/Last'].str.split("$").apply(get_value))
-
-print(m_df['Month'].value_counts(sort=True))
-print(m_df['Year'].value_counts(sort=True, ascending=True))
-print(m_df['Month'].value_counts(sort=True)[10])
-
-ma_df = pd.DataFrame({
-    "Month": ["January", "Feburary", "March", "April", " May", "June", "July", "August", "September", "October", "November", "December"],
-    "Frequency": [m_df['Month'].value_counts(sort=True)[1], m_df['Month'].value_counts(sort=True)[2], m_df['Month'].value_counts(sort=True)[3], m_df['Month'].value_counts(sort=True)[4], m_df['Month'].value_counts(sort=True)[5], m_df['Month'].value_counts(sort=True)[6], m_df['Month'].value_counts(sort=True)[7], m_df['Month'].value_counts(sort=True)[8], m_df['Month'].value_counts(sort=True)[9], m_df['Month'].value_counts(sort=True)[10], m_df['Month'].value_counts(sort=True)[11], m_df['Month'].value_counts(sort=True)[12]]
-                     })
 
 # Create Bar graph
 fig = px.bar(df, x='Fruit',y='Cost')
@@ -46,15 +26,23 @@ fig2 = px.line(gme_df, x='Date_dt', y='Close_num', labels={'Date_dt': 'Date', 'C
 
 fig3 = px.line(gme_df, x = 'Date', y='Close_num')
 
-movie_fig = px.bar(ma_df, x='Month', y= 'Frequency', color= 'Month', color_discrete_sequence=px.colors.qualitative.Set3,   title="Movie Releases by Month")
-
 ''' ***** Layout ****** '''
 app.layout = html.Div(children = [
-    html.H1(children='Hello World'),
-    html.Div(children = 'Dash is cool!'),
+    #Navigation Bar linking to the three graphs
+    html.Header(
+        html.Nav(
+            html.Ul(
+                html.Li(children=[
+                html.A("Fruit Cost", href="#Fruit Cost"),
+                html.A("GME 5 YR", href="#GME 5 year Data Close"),
+                html.A("GME No Datastamps", href="#GME 5 year no DT") ])
+            )
+        )
+    ),
+    html.H1(children='Hello World', className="title"),
+    html.Div(children = 'Dash is cool!', className="title"),
     dcc.Graph(id = 'Fruit Cost', figure = fig),
     dcc.Graph(id = 'GME 5 year Data Close', figure = fig2),
-    dcc.Graph(id= '5000 Movies', figure = movie_fig),
     dcc.Graph(id= 'GME 5 year no DT', figure = fig3)
     
 ])
